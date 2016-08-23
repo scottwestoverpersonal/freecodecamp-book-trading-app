3267492ff9c8693d8306ce7207736388c058fd4b
Scott Westover <scottwestover2006@gmail.com>
2016-08-23 00:11:44 +0000

    removed keys

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 var express = require('express');
-var Yelp = require('yelp');
 var assert = require('assert');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var stormpath = require('express-stormpath');
 var app = express();
 
-var yelp = new Yelp({
-  consumer_key: 'gmnAAe1XgNKeGoRV5knGOw',
-  consumer_secret: 'JO-I347HD8VCV3m3c_eDukbbVrE',
-  token: 'StKbQj9MfQOR7VsSOEL8xXplcRfDfgj7',
-  token_secret: 'z-Wf1BeFvcYb5YxFR2Y_wKcgVvw',
-});
-
-var mongoURL = 'mongodb://heroku_tk3dnhh3:jvru6o4r6rhr7kn35oc7l4ksje@ds013456.mlab.com:13456/heroku_tk3dnhh3';
+var mongoURL = '';
 
 app.use(stormpath.init(app, {
   website: true,
     apiKey: {
-      id: '1P3PA4S39F3IL26KBI1R2AWMY', 
-      secret: 'Eg2b+h/zGdfvTPf3DaJWlCGSLGDYKs4hyzPj5EWPtNw'
+      id: '', 
+      secret: ''
     },
  application: {
-   href: 'https://api.stormpath.com/v1/applications/606aCfu9ELSChkl5nH13k0',
+   href: '',
  }
 }));
 
